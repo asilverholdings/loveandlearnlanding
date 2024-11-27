c2a396125fdb9b6a752bec01c90bba3e24d2c462
antoniofodor <antonio.mateo.fodor@outlook.com>
2024-11-27 11:51:13 -0500

    updated home page

--- deski/components/hero-banner/HeroBannerFour.jsx
+++ deski/components/hero-banner/HeroBannerFour.jsx
@@ -9,6 +9,19 @@ const HeroBannerFour = () => {
       <Image width="10" height="19" src="/images/shape/101.svg" alt="shape" className="shapes shape-five" />
       <Image width="242" height="433" src="/images/shape/102.svg" alt="shape" className="shapes shape-six" />
       <Image width="246" height="400" src="/images/shape/103.svg" alt="shape" className="shapes shape-seven" />
+
+      <div className="container">
+        <div className="header-text">
+          <h2>
+            Love and Learn Nanny Agency
+          </h2>
+          <p>
+          Love and Learn is a nannying agency in <b>Gainesville, FL,</b> created to connect families with fully vetted, 
+          professional nannies trained in their unique curriculum focused on holistic child development.  At Love and Learn, 
+          their mission is to provide care that nurtures growth and fosters learning, ensuring every child thrives. 
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
